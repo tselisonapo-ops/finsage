a62f63251a5e53f92b5beb17a3d78f26d106b5e4
Napo <tselisonapo@gmail.com>
2026-03-22 19:44:21 +0200

    bump bootstrap versin

--- BackEnd/Services/db_service.py
+++ BackEnd/Services/db_service.py
@@ -16539,12 +16539,12 @@ class DatabaseService:
             try:
                 cur.execute("SELECT pg_advisory_xact_lock(%s);", (int(company_id),))
 
-                print(f"RUNNING MIGRATION {schema}:bootstrap v37")
+                print(f"RUNNING MIGRATION {schema}:bootstrap v38")
                 self.execute_ddl(
                     ddl_bootstrap_sql,
                     cur=cur,
                     migration_key=f"{schema}:bootstrap",
-                    migration_version=37,
+                    migration_version=38,
                 )
 
                 print(f"RUNNING MIGRATION {schema}:ap v7")
