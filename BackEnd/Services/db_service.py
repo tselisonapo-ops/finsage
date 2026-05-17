c68c406971ec75edbf0d4f4e3590832fc0df19dd
Napo <tselisonapo@gmail.com>
2026-05-17 20:52:59 +0200

    update modified files

--- BackEnd/Services/db_service.py
+++ BackEnd/Services/db_service.py
@@ -20875,12 +20875,12 @@ class DatabaseService:
             try:
                 cur.execute("SELECT pg_advisory_xact_lock(%s);", (int(company_id),))
 
-                print(f"RUNNING MIGRATION {schema}:bootstrap v58")
+                print(f"RUNNING MIGRATION {schema}:bootstrap v59")
                 self.execute_ddl(
                     ddl_bootstrap_sql,
                     cur=cur,
                     migration_key=f"{schema}:bootstrap",
-                    migration_version=58,
+                    migration_version=59,
                 )
 
                 print(f"RUNNING MIGRATION {schema}:ap v7")
