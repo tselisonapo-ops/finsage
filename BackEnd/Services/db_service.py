c44a912e6b2e0496352ab8645d908596cf7a5d5d
Napo <tselisonapo@gmail.com>
2026-07-17 21:16:35 +0200

    update modified files

--- BackEnd/Services/db_service.py
+++ BackEnd/Services/db_service.py
@@ -82990,8 +82990,9 @@ Intangible assets are derecognised on disposal or when no future economic benefi
             LEFT JOIN {schema}.customers c
             ON c.id = adi.customer_id
 
-            LEFT JOIN {schema}.suppliers s
+            LEFT JOIN {schema}.vendors s
             ON s.id = adi.supplier_id
+            AND s.company_id = adi.company_id
 
             LEFT JOIN {schema}.revenue_contracts rc
             ON rc.id = adi.source_revenue_contract_id
